f001c31c2f679b479f68b45819bdace03c7a3cc0
Christian Duvholt <cxhristian@gmail.com>
2014-10-09 13:57:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
-sudo apt-get install htop supervisor vim python-dev python-pip
+sudo apt-get install htop supervisor vim python-dev python-pip git curl
+sudo rm /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Europe/Oslo /etc/localtime
-sudo echo 'XKBMODEL="pc105"\nXKBLAYOUT="no"' > /etc/default/keyboard
+sudo sh -c "echo 'XKBMODEL=\"pc105\"\nXKBLAYOUT=\"no\"' > /etc/default/keyboard"
 sudo pip install virtualenvwrapper
+echo 'source virtualenvwrapper.sh' >> .bashrc
+source virtualenvwrapper.sh
 cd /home/pi
 git clone https://github.com/appKom/notipi.git
 cd notipi
@@ -9,5 +12,5 @@ git checkout production
 mkvirtualenv notipi
 setvirtualenvproject
 pip install -r requirements.txt
-sudo echo '[program:notipi]\nuser=root\ncommand=/home/pi/.virtualenvs/notipi/bin/python /home/pi/notipi/main.py' > /etc/supervisor/conf.d/notipi.conf
+sudo sh -c "echo '[program:notipi]\nuser=root\ncommand=/home/pi/.virtualenvs/notipi/bin/python /home/pi/notipi/main.py' > /etc/supervisor/conf.d/notipi.conf"
 echo 'Remember to edit settings.py'
