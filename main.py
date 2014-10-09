74a57e6b54e9cc40ca99f12dd889fcaa20237dd3
Christian Duvholt <cxhristian@gmail.com>
2014-10-09 22:07:01 +0200

    Rewrote to use classes

--- main.py
+++ main.py
@@ -9,95 +9,97 @@ except ImportError:
     raise SystemExit('settings.py not found')
 
 
-BUTTON_PIN = 9
-LIGHT_PIN = 10
-LED_PIN = 11
+class Pin(object):
+    URL = "http://passoa.online.ntnu.no/notiwire/" + NAME + '/'
 
-INPUT = 0
-OUTPUT = 1
-
-HIGH = 1
-LOW = 0
-
-PUD_UP = 2
-
-URL = "http://passoa.online.ntnu.no/notiwire/" + NAME + '/'
+    def post(self, data):
+            data['api_key'] = API_KEY
+            r = requests.post(URL + self.relative_url, data=data)
+            if DEBUG:
+                print 'POST:', URL + self.relative_url
 
 
-class Coffee(object):
+class Coffee(Pin):
+    pots = 0
+
+    relative_url = 'coffee'
+
+    def __init__(self, notipi, PIN):
+        self.notipi = notipi
+        self.PIN = PIN
+
+        self.last = time.time()
+        GPIO.setup(self.PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(PIN, GPIO.RISING, callback=self.update, bouncetime=5000)
+
+    def update(self, signal):
+        self.pots += 1
+        self.notipi.blink(2)
+        # Date formatted like '06. October 2014 13:13:19'
+        coffee_date = datetime.datetime.now().strftime('%m. %B %Y %H:%M:%S')
+        self.post(relative_url, {'pots': self.pots, 'datetime': coffee_date})
+        time.sleep(1)
+        self.notipi.blink(2)
+        if DEBUG:
+            print 'New coffee pot:', coffee_date
+
+
+class Light(Pin):
+    relative_url = 'light'
+
+    def __init__(self, notipi, PIN):
+        self.notipi = notipi
+        self.PIN = PIN
+
+        GPIO.setup(self.LIGHT_PIN, GPIO.IN)
+        GPIO.add_event_detect(PIN, GPIO.BOTH, callback=self.update, bouncetime=5000)
+
+    def update(self, signal):
+        print 'Light:', signal
+        # 0 = On, apparantly
+        # if digitalRead(LIGHT_PIN) == 0:
+        #     status = 'on'
+        # else:
+        #     status = 'off'
+        # post('light', {'light': status})
+        # blink()
+        # if DEBUG:
+        #     print 'Light level updated:', status
+
+
+class Led(Pin):
+    def __init__(self, notipi, PIN):
+        self.notipi = notipi
+        self.PIN = PIN
+
+        GPIO.setup(self.PIN, GPIO.OUT)
+
+    def blink(n=1):
+        for _ in range(n):
+            GPIO.output(self.PIN, False)
+            time.sleep(0.3)
+            GPIO.output(self.PIN, True)
+            time.sleep(0.3)
+
+
+class Notipi(object):
+    BUTTON_PIN = 9
+    LIGHT_PIN = 10
+    LED_PIN = 11
+
     def __init__(self):
-        self.pots = 0
-        GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING)
-        GPIO.add_event_callback(BUTTON_PIN, self.update)
+        GPIO.setmode(GPIO.BCM)
+        self.coffee = Coffee(self, BUTTON_PIN)
+        self.light = Light(self, LIGHT_PIN)
+        self.led = Led(self, LED_PIN)
+        self.led.blink(5)
 
-    def update(self):
-        print 'New pot!', self.pots
+    def blink(*args, **kwargs):
+        self.led(*args, **kwargs)
 
 
 def main():
-    setup()
-    counter = 0
-    pots = 0
-    now = time.time()
-    blink(10)
-    coffee = Coffee()
-    while True:
-        # Light
-        if now + 20 < time.time():
-            now = time.time()  # Reset timer
-            update_light()
-
-        # # Coffee
-        # if digitalRead(BUTTON_PIN) == 0:
-        #     pots += 1
-        #     update_coffee(pots)
-
-
-def update_coffee(pots):
-    blink(2)
-    # Date formatted like '06. October 2014 13:13:19'
-    coffee_date = datetime.datetime.now().strftime('%m. %B %Y %H:%M:%S')
-    post('coffee', {'pots': pots, 'datetime': coffee_date})
-    time.sleep(1)
-    blink(2)
-    if DEBUG:
-        print 'New coffee pot:', coffee_date
-
-
-def update_light():
-    # 0 = On, apparantly
-    if digitalRead(LIGHT_PIN) == 0:
-        status = 'on'
-    else:
-        status = 'off'
-    post('light', {'light': status})
-    blink()
-    if DEBUG:
-        print 'Light level updated:', status
-
-
-def post(relative_url, data):
-    data['api_key'] = API_KEY
-    r = requests.post(URL + relative_url, data=data)
-    if DEBUG:
-        print 'POST:', URL + relative_url
-
-
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(LIGHT_PIN, GPIO.IN)
-    GPIO.setup(LED_PIN, GPIO.OUT)
-
-    GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING)
-
-
-def blink(n=1):
-    for _ in range(n):
-        GPIO.output(LED_PIN, LOW)
-        time.sleep(0.3)
-        GPIO.output(LED_PIN, HIGH)
-        time.sleep(0.3)
+    notipi = Notipi()
 
 if __name__ == '__main__':
     main()
